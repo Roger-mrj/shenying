1ca6c1bec8871d722bd959ed2354e0e5bcbcd0ae
landstar <645106533@qq.com>
2020-08-26 18:25:55 +0800

    '神影电影'

---
+++ src/components/CiList/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="cinema_body">
+  
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "CiList"
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Header/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="header">
+    <h1>{{title}}</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "header",
+    props: {
+      title: String,
+      default: '神影电影'
+    }
+  }
+</script>
+
+<style scoped>
+  #header {
+    width: 100%;
+    height: 50px;
+    color: #e54847;
+    border-bottom: 1px solid #e54847;
+    position: relative;
+  }
+  
+  #header h1 {
+    font-size: 18px;
+    text-align: center;
+    line-height: 50px;
+    font-weight: normal;
+  }
+</style>
